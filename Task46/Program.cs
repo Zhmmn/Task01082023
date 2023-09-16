35797b6bc068f744ccad22e04c5d681c0b9b8037
Zhoomart <jabdykasymov@gmail.com>
2023-09-16 22:34:10 +0600

    двумерный массив заполненный случайными числами

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -11,9 +11,9 @@ int[,] CreateMatrixRndint(int rows, int columns, int min, int max)
   int[,] matrix = new int[rows, columns];
   Random rnd = new Random();
 
-  for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < rows; i++)
   {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int j = 0; j < columns; j++)
     {
       matrix[i, j] = rnd.Next(min, max + 1);
     }
