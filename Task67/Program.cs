84915706506308e13b4c51438256a484f2f4f738
Zhoomart <jabdykasymov@gmail.com>
2023-09-19 23:17:52 +0600

    Расчет суммы цифр числа при помощи рекурсии

---
+++ Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1)
+{
+  Console.WriteLine("Некорректный ввод");
+  return;
+}
+
+int SumDigit(int number)
+{
+  if (number == 0) return 0;
+  ;
+  return number % 10 + SumDigit(number / 10);
+}
+int num = SumDigit(number);
+Console.WriteLine($"Сумма цифр числа [{number}]равна {num}");
